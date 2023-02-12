e151832f098c01777d6a4972a5f55b2a2f9257ab
Quentin Fortier <qpfortier@gmail.com>
2023-02-12 16:39:54 +0000

    Update

---
+++ jeux/nim/biparti/nim_biparti.py
@@ -0,0 +1,15 @@
+import python_latex
+from pathlib import Path
+
+picture = []
+for i in range(1, 7):
+    for p in range(2):
+        node = f"${str(i)}_{'A' if p else 'B'}$" 
+        picture.append(f"\\node ({p}{i}) at (-1.5*{i}, {2*p}) {{{node}}};")
+for p  in range(2):
+    for k in range(1, 4):
+        for i in range(1, 6-k+1):
+            color = "red" if p else "black"
+            picture.append(f"\\draw[->, >=latex, color={color}] ({p}{i+k}) to ({1-p}{i});")
+
+python_latex.render("/workspaces/tikz/template.tex", Path(__file__).with_suffix(".tex"), picture='\n'.join(picture))
--- jeux/nim/nim.py
+++ jeux/nim/nim.py
@@ -1,2 +1,11 @@
-with open("nim.tex", "w") as f:
-    f.write(r"
+import python_latex
+from pathlib import Path
+
+picture = []
+for i in range(1, 7):
+    picture.append(f"\\node ({i}) at (-1.5*{i},0) {{{i}}};")
+for k, bend in [(1, ""), (2, "bend left"), (3, "bend right")]:
+    for i in range(1, 6-k+1):
+        picture.append(f"\\draw[->, >=latex] ({i+k}) to[{bend}] ({i});")
+
+python_latex.render("/workspaces/tikz/template.tex", Path(__file__).with_suffix(".tex"), picture='\n'.join(picture))
